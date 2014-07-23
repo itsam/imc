4921cf660b70e86bece03416de107e62c7b824c1
Ioannis Tsampoulatidis <tsampoulatidis@gmail.com>
2014-07-24 00:32:59 +0300

    simple custom field is set

---
+++ administrator/models/fields/multiphoto.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @version     3.0.0
+ * @package     com_imc
+ * @copyright   Copyright (C) 2014. All rights reserved.
+ * @license     GNU AFFERO GENERAL PUBLIC LICENSE Version 3; see LICENSE
+ * @author      Ioannis Tsampoulatidis <tsampoulatidis@gmail.com> - https://github.com/itsam
+ */
+
+defined('JPATH_BASE') or die;
+
+jimport('joomla.html.html');
+jimport('joomla.form.formfield');
+
+/**
+ * Supports an HTML select list of categories
+ */
+class JFormFieldMultiphoto extends JFormField
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var		string
+	 * @since	1.6
+	 */
+	protected $type = 'multiphoto';
+
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return	string	The field input markup.
+	 * @since	1.6
+	 */
+	protected function getInput()
+	{
+		// Initialize variables.
+		$html = array();
+		$html[] = "<h1>" . 'MULTI PHOTO' . "</h1>";
+		return implode($html);
+	}
+}
