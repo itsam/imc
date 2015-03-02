fc2e60981a0ac8f4af56b0e1457a3eccf64389d9
Ioannis Tsampoulatidis <tsampoulatidis@gmail.com>
2015-03-02 03:50:33 +0200

    onAfterNewIssueAdded prepare enqueueMessages

--- plugins/imc/mail_notifier/mail_notifier.php
+++ plugins/imc/mail_notifier/mail_notifier.php
@@ -7,11 +7,35 @@ class plgImcmail_notifier extends JPlugin
 
 	function onAfterNewIssueAdded($model, $validData, $id = null)
 	{
+		//check if issue added from frontend
+		if($id == null){
+			$issueid = $model->getItem()->get('id');
+		} 
+		else {
+			$issueid = $id;
+		}
+
+		//$emails = $model->getItem($issueid)->get('notification_emails');
+		JModelLegacy::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR . '/models');
+		$issueModel = JModelLegacy::getInstance( 'Issue', 'ImcModel' );
+		$emails = $issueModel->getItem($issueid)->get('notification_emails');
+
+		$userid = $model->getItem($issueid)->get('created_by');
+		$username = JFactory::getUser($userid)->name;
+		$useremail = JFactory::getUser($userid)->email;
 		
-		$emails = $model->getItem($id)->get('notification_emails');
-		$recipients = implode(',', $emails);
-		if(empty($emails)) $recipients = "mayor@municipality.gr"; //TEMPORARY FOR DEMO
-		JFactory::getApplication()->enqueueMessage('Notification mail sent to '.$recipients, 'Info');
+		//Prepare email for admins
+		if(empty($emails) || $emails[0] == ''){
+			JFactory::getApplication()->enqueueMessage('Admin notifications for this category are not set', 'Info');
+		}
+		else {
+			$recipients = implode(',', $emails);
+			JFactory::getApplication()->enqueueMessage('Notification mail sent to '.$recipients, 'Info');
+		}
+
+		//Prepare email for user
+		JFactory::getApplication()->enqueueMessage('Notification mail sent to '.$username.' at '.$useremail, 'Info');
+
 	}	
 
 	function onAfterStepModified($model, $validData)
