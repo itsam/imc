aff191320b628a68cc6c2bc41cece8f217ccb6fb
Ioannis Tsampoulatidis <tsampoulatidis@gmail.com>
2015-08-19 17:29:22 +0300

    Token includes random string

--- site/controllers/api.json.php
+++ site/controllers/api.json.php
@@ -21,8 +21,8 @@ require_once JPATH_COMPONENT_SITE . '/models/tokens.php';
  * e.g. $ sudo php5enmod mcrypt
  *
  * Every request should contain token, m_id, l
- * where *token* is the m-crypted "json_encode(array)" of username, password, timestamp in the following form:
- * {'u':'username','p':'plain_password','t':'1439592509'}
+ * where *token* is the m-crypted "json_encode(array)" of username, password, timestamp, randomString in the following form:
+ * {'u':'username','p':'plain_password','t':'1439592509','r':'i452dgj522'}
  * all casted to strings including the UNIX timestamp time()
  * where *m_id* is the modality ID according to the REST/API key definition in the administrator side
  * where *l* is the 2-letter language code used for for the responses translation (en, el, de, es, etc)
@@ -91,7 +91,7 @@ class ImcControllerApi extends ImcController
             throw new Exception('Token is invalid');
         }
 
-        if(!isset($objToken->u) || !isset($objToken->p) || !isset($objToken->t)) {
+        if(!isset($objToken->u) || !isset($objToken->p) || !isset($objToken->t) || !isset($objToken->r)) {
             throw new Exception('Token is not well formatted');
         }
 
@@ -130,9 +130,45 @@ class ImcControllerApi extends ImcController
 
 	public function issues()
 	{
+		$result = null;
+		$app = JFactory::getApplication();
 		try {
-		    self::validateRequest();
-			$result = array('foo'=>'bar', 'moo'=>'koo', 'bar'=>231);
+		    $userid = self::validateRequest();
+		    //$userid = 569;
+			//get necessary arguments
+			$minLat = $app->input->getFloat('minLat', null);
+			$maxLat = $app->input->getFloat('maxLat', null);
+			$minLng = $app->input->getFloat('minLng', null);
+			$maxLng = $app->input->getFloat('maxLng', null);
+			$limit = $app->input->getInt('limit', 0);
+
+            //get issues model
+            $issuesModel = JModelLegacy::getInstance( 'Issues', 'ImcModel', array('ignore_request' => true) );
+
+			if(is_null($minLat) || is_null($maxLat) || is_null($minLng) || is_null($maxLng))
+			{
+				//TODO: set state so as to get only allowable issues according to userid
+				$data = $issuesModel->getItems();
+				$result = ImcFrontendHelper::sanitizeIssues($data, $userid);
+			}
+			else
+			{
+				$data = $issuesModel->getItemsInBoundaries($minLat, $maxLat, $minLng, $maxLng);
+				$result = ImcFrontendHelper::sanitizeIssues($data, $userid);
+			}
+
+			//apply restrictions
+			foreach($result as $issue)
+			{
+                if(!$issue->myIssue && $issue->moderation){
+                    unset($issue);
+                }
+                if($issue->state != 1){
+                    unset($issue);
+                }
+			}
+			$result = array_values($result);
+
 			echo new JResponseJson($result, 'Issues fetched successfully');
 		}
 		catch(Exception $e)	{
@@ -163,18 +199,17 @@ class ImcControllerApi extends ImcController
                     if(!is_object($data)){
                         throw new Exception('Issue do not exists');
                     }
-                    $data->myIssue = ($data->created_by == $userid);
-                    $data->moderation = (boolean)$data->moderation;
 
-                    //check for restrictions
-                    if(!$data->myIssue && $data->moderation){
+                    $result = ImcFrontendHelper::sanitizeIssue($data, $userid);
+
+                    //check for any restrictions
+                    if(!$result->myIssue && $result->moderation){
                         throw new Exception('Issue is under moderation');
                     }
-                    if($data->state != 1){
+                    if($result->state != 1){
                         throw new Exception('Issue is not published');
                     }
 
-                    $result = ImcFrontendHelper::sanitizeIssue($data);
                 break;
                 //create new issue
                 case 'POST':
@@ -193,7 +228,7 @@ class ImcControllerApi extends ImcController
                     throw new Exception('HTTP method is not supported');
             }
 
-            echo new JResponseJson($result);
+            echo new JResponseJson($result, 'Issue fetched successfully');
 		}
 		catch(Exception $e)	{
 			echo new JResponseJson($e);
