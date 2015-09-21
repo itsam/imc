ae822737f74406541b225890fd37d0c256460a57
Ioannis Tsampoulatidis <tsampoulatidis@gmail.com>
2015-09-21 14:10:41 +0300

    On new issue move Save/Cancel buttons to bottom

--- site/views/issueform/tmpl/default.php
+++ site/views/issueform/tmpl/default.php
@@ -85,10 +85,6 @@ if(!$canState && $this->item->id > 0) {
 <div class="row">
 <div class="issue-edit front-end-edit">
     <form id="form-issue" action="<?php echo JRoute::_('index.php?option=com_imc&task=issue.save'); ?>" method="post" class="form-validate form-horizontal" enctype="multipart/form-data">
-    <div style="float:right;">
-        <button type="submit" class="validate btn btn-primary"><i class="icon-ok"></i> <?php echo JText::_('JSUBMIT'); ?></button>
-        <a class="btn" href="<?php echo JRoute::_('index.php?option=com_imc&task=issueform.cancel'); ?>" title="<?php echo JText::_('JCANCEL'); ?>"><?php echo JText::_('JCANCEL'); ?></a>
-    </div>
 
     <?php if (!empty($this->item->id)): ?>
         <h1><i class="icon-pencil"></i> <?php echo JText::_('COM_IMC_ISSUE_EDIT'); ?> #<?php echo $this->item->id; ?></h1>
@@ -227,11 +223,15 @@ if(!$canState && $this->item->id > 0) {
 	    </script>
 	<?php endif; ?>
 
-
-    
     <input type="hidden" name="option" value="com_imc" />
     <input type="hidden" name="task" value="issueform.save" />
     <?php echo JHtml::_('form.token'); ?>
+
+    <div style="float:right; padding-bottom: 40px;">
+	    <button type="submit" class="validate btn btn-primary"><i class="icon-ok"></i> <?php echo JText::_('JSUBMIT'); ?></button>
+	    <a class="btn" href="<?php echo JRoute::_('index.php?option=com_imc&task=issueform.cancel'); ?>" title="<?php echo JText::_('JCANCEL'); ?>"><?php echo JText::_('JCANCEL'); ?></a>
+    </div>
+
 
     </form>
 </div>
