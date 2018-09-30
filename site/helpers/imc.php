3554df5a072394a2f1b4463dfa946c8b466a4df4
Ioannis Tsampoulatidis <tsampoulatidis@gmail.com>
2018-09-30 22:16:24 +0300

    Add searchByKey helper function

--- site/helpers/imc.php
+++ site/helpers/imc.php
@@ -1619,4 +1619,20 @@ class ImcFrontendHelper
 	    return $result;
 	}
 	
+	public static function searchByKey($array, $key) {
+		$results = array();
+
+		if(is_array($array)) {
+				if(isset($array[$key])) {
+						$results[] = $array;
+				}
+
+				foreach($array as $subarray) {
+						$results = array_merge($results, self::searchByKey($subarray, $key));
+				}
+		}
+
+		return $results;
+	}
+
 }
