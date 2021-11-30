348315cb22a378e54f00ca1feee70651e9d2761d
Ioannis Tsampoulatidis <tsampoulatidis@gmail.com>
2021-11-30 22:41:34 +0200

    type casting minor fix

--- site/models/fields/multiphoto/server/UploadHandler.php
+++ site/models/fields/multiphoto/server/UploadHandler.php
@@ -476,7 +476,8 @@ class UploadHandler
             $name = $this->upcount_name($name);
         }
         // Keep an existing filename if this is part of a chunked upload:
-        $uploaded_bytes = $this->fix_integer_overflow(intval($content_range[1]));
+        ///$uploaded_bytes = $this->fix_integer_overflow(intval($content_range[1]));
+        $uploaded_bytes = $this->fix_integer_overflow((int)@$content_range[1]);
         while (is_file($this->get_upload_path($name))) {
             if ($uploaded_bytes === $this->get_file_size(
                 $this->get_upload_path($name)
